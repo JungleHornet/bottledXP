7ae11343900897377945c3dc5ac8c3ebfd7bc7ac
JungleHornet <86211409+JungleHornet@users.noreply.github.com>
2025-01-05 20:04:57 -0600

    added stacking to 16 for filled and unfilled capsules

---
+++ src/main/java/games/luminance/bottledxp/BottledXP.java
@@ -0,0 +1,59 @@
+package games.luminance.bottledxp;
+
+import com.mojang.logging.LogUtils;
+import games.luminance.bottledxp.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BottledXP.MODID)
+public class BottledXP
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "bottledxp";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public BottledXP(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.XP_CAPSULE);
+        }
+    }
+
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+}
--- src/main/java/games/luminance/bottledxp/item/ModItems.java
+++ src/main/java/games/luminance/bottledxp/item/ModItems.java
@@ -11,9 +11,9 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BottledXP.MODID);
 
-    public static final RegistryObject<Item> XP_CAPSULE = ITEMS.register("capsule", () -> new CapsuleItem(new Item.Properties().rarity(Rarity.RARE).setNoRepair()));
+    public static final RegistryObject<Item> XP_CAPSULE = ITEMS.register("capsule", () -> new CapsuleItem(new Item.Properties().rarity(Rarity.RARE).stacksTo(16).setNoRepair()));
 
-    public static final RegistryObject<Item> FILLED_CAPSULE = ITEMS.register("filled_capsule", () -> new FilledCapsuleItem(new Item.Properties().rarity(Rarity.RARE).stacksTo(1).setNoRepair()));
+    public static final RegistryObject<Item> FILLED_CAPSULE = ITEMS.register("filled_capsule", () -> new FilledCapsuleItem(new Item.Properties().rarity(Rarity.RARE).stacksTo(16).setNoRepair()));
 
     public static final RegistryObject<Item> ADVANCED_CAPSULE = ITEMS.register("advanced_capsule", () -> new AdvancedCapsuleItem(new Item.Properties().rarity(Rarity.RARE).setNoRepair()));
 
